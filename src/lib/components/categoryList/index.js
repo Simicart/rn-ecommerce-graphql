ddcd292f3cde0461b77abc0a6510e29f0579a355
Zen-net <allyofjustice1@gmail.com>
2020-11-03 18:28:37 +0700

    fix: add icon to subcategories in CategoryList
    Currently I haven't found a way to push svg Icon in project without manual linking.
    So currently we resort to Asciiz Emoji-s, or Images.
    Added a svg to address this issue later on.

--- src/lib/components/categoryList/index.js
+++ src/lib/components/categoryList/index.js
@@ -1,130 +1,148 @@
 import React, {useMemo, useState} from 'react';
-import {ScrollView, Text, View, StyleSheet, Button, TouchableOpacity} from "react-native";
-import {useCatalogContext} from "../../..";
-import {SpaceBlock} from "../others/spaceBlock.js";
-import Icon from 'react-native-ionicons'
-
+import {ScrollView, Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
+import {useCatalogContext} from '../../..';
+import {SpaceBlock} from '../others/spaceBlock.js';
 
 function CategoryList(props) {
-    const {name, data, handleChangePage} = props;
-    const {} = data ?? {};
+  const {name, data, handleChangePage} = props;
+  const {} = data ?? {};
 
+  return (
+      <View>
+        <Text style={styles.categoryTitle}>{name ?? 'Such empty'}</Text>
+
+        <ScrollView style={styles.subCategoriesOutline}>
+          {data.map((x, index) => {
+            return (
+                <TouchableOpacity key={index}
+                                  style={styles.subCategoryContainer}
+                                  onPress={() => handleChangePage(x.id)}
+                >
+                  <Text style={styles.subCategoryText}
+                  >{`${x.name}`}</Text>
+                  <Text style={styles.arrowIcon}>{'>'}</Text>
+                  {/*<Icon ios="ios-add" android="md-add"/>*/}
+                </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+  );
+}
+
+function CategoryDumpComponent(props) {
+  const {id = null} = props;
+  const [catalogState, catalogAction] = useCatalogContext();
+  const {rootCategoryId, categories} = catalogState;
+
+  const renderLayer = categories[id ?? rootCategoryId];
+  if (renderLayer) {
+    return (
+        <CategoryList name={renderLayer.name}
+                      data={renderLayer.children}
+                      handleChangePage={(id: string) => {
+                      }}
+        />
+    );
+  }
+  else {
+    console.log('No children. Should jump to product list?');
     return (
         <View>
-            <Text style={styles.categoryTitle}>{name ?? 'Such empty'}</Text>
 
-            <ScrollView style={styles.subCategoriesOutline}>
-                {data.map((x, index) => {
-                    return (
-                        <TouchableOpacity key={index}
-                                          style={styles.subCategoryContainer}
-                                          onPress={() => handleChangePage(x.id)}
-                        >
-                            <Text style={styles.subCategoryText}
-                            >{`${x.name}`}</Text>
-                            <Text style={styles.arrowIcon}>🧼</Text>
-                            <Icon ios="ios-add" android="md-add" />
-                        </TouchableOpacity>
-                    )
-                })}
-            </ScrollView>
         </View>
     );
+  }
+
+  // no children ?? maybe last layer before real products
+
 }
 
 function CategoryWrapper(props) {
-    const [catalogState,] = useCatalogContext();
-    const {categories, rootCategoryId} = catalogState;
+  const [catalogState] = useCatalogContext();
+  const {categories, rootCategoryId} = catalogState;
 
-    const [currentID, setCurrentID] = useState(props.id || rootCategoryId);
+  const [currentID, setCurrentID] = useState(props.id || rootCategoryId);
 
+  const getDisplayData = (id: string): Array<> => {
+    console.log('getting ' + id);
 
-    const getDisplayData = (id: string): Array<> => {
-        console.log('getting ' + id)
+    const {children} = categories[id] ?? {};
 
-        const {children} = categories[id] ?? {};
-
-        if (!children) {
-            console.info('no children. This should jump to Product list?')
-            //TODO: In this case, switch to Product list
-            return []
-        } else {
-            return children
-                //         get children data
-                .map(children_id => {
-                    return categories[children_id]
-                })
-                // remove children with no data
-                .filter(x => !!x)
-        }
-
+    if (!children) {
+      console.info('no children. This should jump to Product list?');
+      //TODO: In this case, switch to Product list
+      return [];
     }
-    const name: string = categories[currentID]?.name ?? 'Such empty';
+    else {
+      return children
+          //         get children data
+          .map(children_id => {
+            return categories[children_id];
+          })
+          // remove children with no data
+          .filter(x => !!x);
+    }
 
-    const renderData = useMemo(() => getDisplayData(currentID),
-        [currentID, categories, rootCategoryId, name]
-    );
+  };
+  const name: string = categories[currentID]?.name ?? 'Such empty';
 
-    return (
-        <ScrollView style={styles.categoryOutline}>
-            {/*<Text>This did render</Text>*/}
-            {/*/!*<Text>{JSON.stringify(categories, null, 2)}</Text>*!/*/}
-            {/*<SpaceBlock value={50}/>*/}
-            {/*<LineSeparator/>*/}
+  const renderData = useMemo(() => getDisplayData(currentID),
+      [currentID, categories, rootCategoryId, name],
+  );
 
-
-            {console.log(renderData)}
-            <CategoryList name={name} data={renderData} handleChangePage={setCurrentID}/>
-            <SpaceBlock/>
-            <Button title={'RESET'} onPress={() => setCurrentID(rootCategoryId)}/>
-        </ScrollView>
-    );
+  return (
+      <ScrollView style={styles.categoryOutline}>
+        <CategoryList name={name} data={renderData} handleChangePage={setCurrentID}/>
+        <SpaceBlock/>
+        <Text>{JSON.stringify({})}</Text>
+        <Button title={'RESET'} onPress={() => setCurrentID(rootCategoryId)}/>
+      </ScrollView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    categoryTitle: {
-        fontSize: 16,
-        fontWeight: "500",
-        lineHeight: 19,
-        letterSpacing: 0,
-    },
-    categoryOutline: {
-        marginTop: 63,
-        marginLeft: 8,
-        marginRight: 8,
-    },
-    subCategoriesOutline: {
-        marginTop: 12,
-    },
-    subCategoryContainer: {
-        marginTop: 18,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cdc9c3',
-        display: 'flex',
-        flexDirection: 'row'
-    },
-    subCategoryText: {
-        fontSize: 14,
-        letterSpacing: 0,
-        lineHeight: 16,
-        marginTop: 3,
-        marginBottom: 14,
-        flex: 33,
-    },
-    arrowIcon: {
-        flex: 3,
-        fontSize: 20,
-        letterSpacing: 0,
-        lineHeight: 23,
-        color: '#E0E0E0'
-    }
+  categoryTitle: {
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 19,
+    letterSpacing: 0,
+  },
+  categoryOutline: {
+    marginTop: 63,
+    marginLeft: 8,
+    marginRight: 8,
+  },
+  subCategoriesOutline: {
+    marginTop: 12,
+  },
+  subCategoryContainer: {
+    marginTop: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cdc9c3',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  subCategoryText: {
+    fontSize: 14,
+    letterSpacing: 0,
+    lineHeight: 16,
+    marginTop: 3,
+    marginBottom: 14,
+    flex: 33,
+  },
+  arrowIcon: {
+    flex: 3,
+    fontSize: 24,
+    letterSpacing: 0,
+    lineHeight: 23,
+    color: '#E0E0E0',
+  },
 
-})
-
+});
 
 export default CategoryWrapper;
 
-export {CategoryList, CategoryWrapper};
+export {CategoryList, CategoryWrapper, CategoryDumpComponent};
 
 
