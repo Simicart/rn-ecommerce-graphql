c1bb04140eef624e398ed4c821005b997fd755bf
lanapone <lanapone@lanapones-MacBook-Pro.local>
2020-11-05 19:43:59 -0800

    fix: useCategory only make network request if no id

--- src/talon/category/useCategory.js
+++ src/talon/category/useCategory.js
@@ -1,17 +1,24 @@
-import { useQuery, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import React, { useState } from 'react';
+import { useCatalogContext } from '../..';
 
 export const useCategory = (props) => {
-  const { categoryId, query, updateCategories } = props;
+  const { categoryId, query, updateCategories, forced = false } = props;
+
+  const [catalogState] = useCatalogContext();
+  const { categories } = catalogState;
 
   // Avoid repeating request
   const [skip, setSkip] = useState(false);
+
+  // if data already in redux ---> end
+  setSkip(!skip && categories.hasOwnProperty(categoryId) && !forced);
 
   const { loading, error, data } = useQuery(query, { skip: skip });
 
   if (data && data.category) {
     updateCategories(data.category);
-    // Note: update currentPage, rootCategoryPage?
+    // TODO: update currentPage, rootCategoryPage?
   }
 
   return {
